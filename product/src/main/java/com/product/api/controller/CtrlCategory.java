8a27852fb1c95c8962561b9719f1221586679e8f
JSR-Mario <juanmario@ciencias.unam.mx>
2026-03-13 09:06:39 -0600

    moidfy(Injections): change to field injections rather than constructor injections

--- product/src/main/java/com/product/api/controller/CtrlCategory.java
+++ product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -2,6 +2,7 @@ package com.product.api.controller;
 
 import com.product.api.entity.Category;
 import com.product.api.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,18 @@ import java.util.List;
 @RequestMapping("/category")
 public class CtrlCategory {
 
-    private final CategoryService categoryService;
+    // Inyección de dependencias por constructor
+    /*
+     * private final CategoryService categoryService;
+     * 
+     * public CtrlCategory(CategoryService categoryService) {
+     * this.categoryService = categoryService;
+     * }
+     */
 
-    public CtrlCategory(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
+    // Inyección de dependencias por campo
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping
     public List<Category> getCategories() {
--- product/src/main/java/com/product/api/service/CategoryServiceImpl.java
+++ product/src/main/java/com/product/api/service/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.product.api.service;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.product.api.entity.Category;
 import com.product.api.repository.CategoryRepository;
@@ -8,14 +9,21 @@ import com.product.api.repository.CategoryRepository;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private final CategoryRepository repoCategory;
+    // Inyección de dependencias por constructor
+    /*
+     * private final CategoryRepository repoCategory;
+     * 
+     * public CategoryServiceImpl(CategoryRepository repoCategory) {
+     * this.repoCategory = repoCategory;
+     * }
+     */
 
-    public CategoryServiceImpl(CategoryRepository repoCategory) {
-        this.repoCategory = repoCategory;
-    }
+    // Inyección de dependencias por campo
+    @Autowired
+    private CategoryRepository repoCategory;
 
     @Override
-    public List<Category> getCategories(){
+    public List<Category> getCategories() {
         return repoCategory.findAllByOrderByCategoryAsc();
     }
 }
