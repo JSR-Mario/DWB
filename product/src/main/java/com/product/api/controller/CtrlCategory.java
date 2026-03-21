26adaebc0a6ab17f7656410740bf2ce63c2d19fc
JSR-Mario <juanmario@ciencias.unam.mx>
2026-03-20 21:46:51 -0600

    feat(c8): add c8 recomendations
    - controller with full CRUD
    - error handling for valid entries

--- product/src/main/java/com/product/api/controller/CtrlCategory.java
+++ product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -2,12 +2,17 @@ package com.product.api.controller;
 
 import com.product.api.entity.Category;
 import com.product.api.service.CategoryService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.product.api.dto.DtoCategoryIn;
 import com.product.api.dto.Response;
 import java.util.List;
 
@@ -30,6 +35,31 @@ public class CtrlCategory {
 
     @GetMapping
     public ResponseEntity<Response<List<Category>>> getCategories() {
-        return new ResponseEntity<>(categoryService.findAll(), HttpStatus.OK);
+        return ResponseEntity.ok(categoryService.findAll());
+    }
+
+    @GetMapping("/active")
+    public ResponseEntity<List<Category>> findActiveCategories() {
+        return ResponseEntity.ok(categoryService.findActive());
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> createCategory(@Valid @RequestBody DtoCategoryIn in) {
+        return ResponseEntity.ok(categoryService.create(in));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> updateCategory(@Valid @RequestBody DtoCategoryIn in, @PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.update(in, id));
+    }
+
+    @PatchMapping("/{id}/enable")
+    public ResponseEntity<ApiResponse> enableCategory(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.enable(id));
+    }
+
+    @PatchMapping("/{id}/disable")
+    public ResponseEntity<ApiResponse> disableCategory(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.disable(id));
     }
 }
