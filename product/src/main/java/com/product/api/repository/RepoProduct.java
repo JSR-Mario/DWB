c87849f5fc34fec9a92e87063ae5d773c11c9e0e
JSR-Mario <juanmario@ciencias.unam.mx>
2026-04-22 00:28:56 -0600

    update(product detail): add product detail endpoint using C12

--- product/src/main/java/com/product/api/repository/RepoProduct.java
+++ product/src/main/java/com/product/api/repository/RepoProduct.java
@@ -1,11 +1,22 @@
 package com.product.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.product.api.entity.Product;
+import com.product.api.dto.out.DtoProductOut;
 
 @Repository
 public interface RepoProduct extends JpaRepository<Product, Integer> {
+
+	@Query("SELECT new com.product.api.dto.out.DtoProductOut("
+			+ "p.product_id, p.gtin, p.product, p.description, p.price, p.stock, p.status, "
+			+ "p.category_id, c.category) "
+			+ "FROM Product p "
+			+ "JOIN Category c ON p.category_id = c.categoryID "
+			+ "WHERE p.product_id = :id")
+	DtoProductOut getProduct(@Param("id") Integer id);
 
 }
